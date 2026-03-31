775fcf84ae89bc98091be7d19b8845c68b6e4940
dilshadalmm <nationalpathshala@gmail.com>
2026-03-31 20:17:46 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
